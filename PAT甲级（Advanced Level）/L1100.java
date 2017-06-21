1d97b37db1d1041dd148b9010d678b3011937376
LiQiaoQiao <1240822303@qq.com>
2017-06-22 00:01:06 +0800

    -

---
+++ PAT甲级（Advanced Level）/L1100.java
@@ -0,0 +1,28 @@
+package advanced;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class L1100 {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+	    int n=scan.nextInt();
+	    scan.nextLine();
+	    String [] ge = {"tret","jan","feb", "mar", "apr", "may", "jun", "jly", "aug", "sep", "oct", "nov", "dec"};
+	    String [] gao = {"tam","hel","maa","huh","tou","kes","hei","elo","syy","lok","mer","jou"};
+	    for(int i=0;i<n;i++) {
+	      String s = scan.nextLine();
+	      if(s.matches("[0-9]*")) {
+	    	  
+	      }else {
+	    	  int [] a=new int [2*n+1];
+	      }
+	    }
+	}
+	
+	
+
+	
+	
+}
